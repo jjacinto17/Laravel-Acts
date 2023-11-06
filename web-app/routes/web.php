aa4faa2db4a98c6d76512667562ab232e115892a
jjacinto17 <jjacinto_20@alu.uabcs.mx>
2023-11-06 14:54:13 -0700

    Actividad 4

---
+++ web-app/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return "<h1> Hola </h1>";
+});
+
+Route::get('/{saludo}/{user}/{userA?}', function ($saludo, $user, $userA = null) {
+    if ($saludo === "saludo") {
+        if (preg_match('/^[A-Za-z]+$/', $user)) {
+            if ($userA !== null) {
+                return "<h1>Hola " . $user . " " . $userA . "</h1>";
+            } else {
+                return "<h1>Hola " . $user . "</h1>";
+            }
+        } else {
+            return "No se permiten números en el nombre de usuario.";
+        }
+    }
+})->where('user', '[A-Za-z]+')->where('userA', '[A-Za-z]+');
+
+Route::get('/{operacion}/{num1}/{num2}', function ($operacion, $num1, $num2) {
+    if ($operacion === "suma") {
+
+        return "<h1>Resultado: " . $num1 + $num2 . "</h1>";
+
+    } elseif ($operacion === "resta") {
+
+        return "<h1>Resultado: " . $num1 - $num2 . "</h1>";
+
+    } elseif ($operacion === "multiplicacion") {
+
+        return "<h1>Resultado: " . $num1 * $num2 . "</h1>";
+
+    } elseif ($operacion === "division") {
+
+        if ($num2 != 0) {
+            return "<h1>Resultado: " . $num1 / $num2 . "</h1>";
+        } else {
+            return "No es posible dividir entre cero.";
+        }
+
+    } else {
+        return "<h1> Operación incorrecta </h1>" ;
+    }
+});
+
